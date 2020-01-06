31b94a1d07c8612fa43f0acccd8b02e88e5e3310
Julia Linde <julialinde@wcw-stud-145-109-32-216.wireless.uva.nl>
2020-01-06 14:03:42 +0100

    Begin python script

--- path.py
+++ path.py
@@ -5,3 +5,28 @@ Tom Kamstra, Izhar Hamer, Julia Linde
 
 Finds the optimal paths between the chips
 """
+
+import csv
+
+data = open("netlist_1.csv")
+reader = csv.reader(data)
+
+netlist = []
+
+for net_1, net_2 in reader:
+    listje = (net_1, net_2)
+    netlist.append(listje)
+    
+print(netlist)
+
+gates = open("pritn_1.csv")
+reader = csv.reader(gates)
+
+gate_coordinates = []
+
+for number, x, y in reader:
+    listje = [x, y, 0]
+    gate_coordinates.append(listje)
+
+print("GATE COORDINATES")
+print(gate_coordinates)
