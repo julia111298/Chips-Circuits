1cbccb5e58fd6028858cb59dcdf7d87fe745d6d5
AllTheLonelyPeople <just_izhar@live.com>
2020-01-13 13:11:35 +0100

    BFS works on a 3d grid now

--- testsearch/BFS3d.py
+++ testsearch/BFS3d.py
@@ -55,33 +55,59 @@ class Graph():
                         node2.distance = node1.distance + 1
 
 
-grid = []
-for x in range(2):
-    for y in range(2):
-        grid.append((x,y))
+grid1 = []
+for x in range(3):
+    for y in range(3):
+        for z in range(3):
+            grid1.append((x,y,z))
 
 g = Graph()
-a = Vertex("(0, 0)")
+a = Vertex("(0, 0, 0)")
 g.add_vertex(a)
 
 
-for i in grid:
+for i in grid1:
     g.add_vertex(Vertex(str(i)))
     
 
 grid2 = []
-for i in grid:
+grid3 = []
+for i in grid1:
     grid2.append(i)
+    grid3.append(i)
 
 edges = []
-for i in grid:
+print(grid1)
+# for i in grid1:
+#     for j in grid2:
+#         for k in grid3:
+#             if abs(j[0] - i[0] - k[0]) == 1 and j[1] - i[1] - k[1] == 0 and k[2] - j[2] - i[2] == 0:
+#                 if (i,j,k) not in edges:
+#                     edges.append((i,j,k))
+#             elif abs(j[1] - i[1] - k[1]) == 1 and j[0] - i[0] - k[0] == 0 and k[2] - j[2] - i[2] == 0:
+#                 if (i,j,k) not in edges:
+#                     edges.append((i,j,k))
+#             elif abs(j[2] - i[2] - k[2]) == 1 and j[0] - i[0] - k[0] == 0 and k[1] - j[1] - i[1] == 0:
+#                 if (i,j,k) not in edges:
+#                     edges.append((i,j,k))
+
+for i in grid1:
     for j in grid2:
-        if abs(j[0] - i[0]) == 1 and j[1] - i[1] == 0:    
+        if abs(j[0] - i[0]) == 1 and j[1] - i[1] == 0 and j[2] - i[2] ==0:    
             if (j,i) not in edges:
                 edges.append((i,j))
-        elif abs(j[1] - i[1]) == 1 and j[0] - i[0] == 0:
+        elif abs(j[1] - i[1]) == 1 and j[0] - i[0] == 0 and j[2] - i[2] == 0:
             if (j,i) not in edges:
                 edges.append((i,j))
+        elif abs(j[2] - i[2]) == 1 and j[0] - i[0] == 0 and j[1] - i[1] == 0:
+            if (j,i) not in edges:
+                edges.append((i,j))
+print("#@ edges: ", len(edges))
+
+# for i in edges:
+#     g.add_edge(str(i[0]), str(i[1]), str(i[2]))
+
+# edges = ["(0, 0, 0) (0, 1, 0)", "(0, 0, 0) (1, 0, 0)", "(0, 0, 0) (0, 0, 1)", "(1, 0, 0) (1, 1, 0)", "(1, 0, 0) (1, 0, 1)", "(0, 1, 0) (0, 1, 1)", "(0, 1, 0) (1, 1, 0)", "(1, 1, 0) (1, 1, 1)"]
 for i in edges:
     g.add_edge(str(i[0]), str(i[1]))
 
