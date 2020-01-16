716fde8ed6d989c443dd16788c8cb4114db1e329
TomKamstra <tomk2000@hotmail.nl>
2020-01-16 13:07:47 +0100

    Added better heuristic function

--- code/algorithms/aStar.py
+++ code/algorithms/aStar.py
@@ -1,11 +1,12 @@
-def heuristic(h1, h2):
-    (x1, y1) = h1
-    (x2, y2) = h2
+
+def get_heuristic(gate_start, gate_end): 
+    x1, y1 = gate_start[0], gate_start[1]
+    x2, y2 = gate_end[0], gate_end[1]
     return abs(x1 - x2) + abs(y1 - y2)
 
 def search(graph, begin, end):
     border = PriorityQueue()
-    border,put(begin, 0)
+    border, put(begin, 0)
     cameFrom = {}
     costUpdate = {}
     cameFrom[begin] = None
@@ -21,7 +22,9 @@ def search(graph, begin, end):
             newCost = costUpdate[current] + graph.cost(current, new)
             if new not in costUpdate or newCost < costUpdate[new]:
                 costUpdate[new] = newCost
-                lead = newCost + heuristic(end, new)
+                lead = newCost + get_heuristic(end, new)
                 border.put(new, lead)
                 cameFrom[new] = current
     return cameFrom, costUpdate
+
+print(get_heuristic([0, 1, 0], [1, 1, 0]))
