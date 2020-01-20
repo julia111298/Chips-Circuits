09ea2bb108fe5509635dcd4a1ad2ee9a4e8e9de4
AllTheLonelyPeople <just_izhar@live.com>
2020-01-20 13:27:38 +0100

    Fixed bugs still not working

--- code/algorithms/Astar_2.py
+++ code/algorithms/Astar_2.py
@@ -4,6 +4,7 @@
 # By team De Madarijntjes
 # 
 # Pseudocode source: https://www.geeksforgeeks.org/a-search-algorithm/
+# and http://mat.uab.cat/~alseda/MasterOpt/AStar-Algorithm.pdf
 ##################################################
 class Grid():
     def __init__(self, grid_size):
@@ -24,13 +25,19 @@ class Node():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.g = 0
 
     def f_score(self):
         return self.g + self.h
     
-    def g_score(self, value):
-        self.g = value
+    def get_g(self):
         return self.g
+    
+    def set_g(self, value):
+        self.g = value
+    
+    def g_score(self, value):
+        self.g += value
     
     def h_score(self, current, goal):
         self.h = abs(current.x - goal.x) + abs(current.y - goal.y)
@@ -61,22 +68,26 @@ grid = grid.get_grid()
 
 start = Node(0, 0)
 goal = Node(2, 2)
-start.g_score(0)
-start.h_score(start, Node(1, 1))
+start.h_score(start, goal)
 
 open_list = [start]
 closed_list = []
 
 # Make sure successors aren't f'ed up
 blocked = []
-g_counter = 0
+g_counter = 1
 
 path = []
 
+node_previous = []
+temp = 0
+
 while open_list:
     
-
+    
     # q = min(open_list, key=)
+
+    # Alle opties met de minimum f waarde pakken?hi
     f_list = []
     for i in open_list:
         f_list.append(i.f_score())
@@ -84,40 +95,75 @@ while open_list:
     for i in range(len(f_list)):
         if f_list[i] == minimum:
             q = open_list[i]
-
+    # print("QQQ", q)
+    temp += 1
+    if temp == 3:
+        break
     
     if str(q) == str(goal):
         print(path)
         print("finished")
+        break
 
-    open_list.remove(q)
-    closed_list.append(q)
+    # open_list.remove(q)
+    # closed_list.append(q)
     
+    # blocked.append(q)
+    # print("closed: : :", closed_list)
+    successors = q.successors(grid, q, closed_list)
     
-    blocked.append(q)
-    successors = q.successors(grid, q, blocked)
+    # blocked.append(q)
+    # print("blcoked,: ", blocked)
+    # print("prev", node_previous)
+
+    if node_previous:
+        successors.pop(0)
+    
+    print(q, " successors: ", successors)
+    node_previous.clear()
+    node_previous.append(q)
+        
+
+
+    # print("open!", open_list)
 
     for i in successors:
-        g_counter += 1
-        if i in closed_list:
-            continue
-        i.g_score(g_counter)
-        i.h_score(i, q)
-        f = i.f_score()
-        if i in open_list and f < i.g_score(i.g_score(g_counter) + 1):
-            # path.append(q)
-            print("true")
-            open_list.remove(i)
+        # g_counter += 1
 
-        elif i in closed_list and f < i.g_score(i.g_score(g_counter) + 1):
-            print("JOEJOE")
+        # i.g_score(1)
+        # i.h_score(i, q)
+        # f = i.f_score()
+
+        successor_current_cost = q.get_g()
+        print("i: ", i)
+        print("open lisT", open_list, "closed:", closed_list)
+        if i in open_list:
+            print("getggg: ", i.get_g())
+            print("current cost succ: ", successor_current_cost)
+            if i.get_g() <= successor_current_cost:
+                # path.append(i)
+                # print("true")
+                # open_list.remove(i)
+                break
+
+        elif i in closed_list:
+            print("in closed")
+            if i.get_g() <= successor_current_cost:
+                break
             closed_list.remove(i)
-        
-        elif i not in open_list and i not in closed_list:
             open_list.append(i)
-            i.g_score(f)
+        
+        else:
+            open_list.append(i)
+            # i.g_score(1)
+            # i.h_score(i, goal)
+            # i.f_score()
             i.h_score(i, goal)
-            i.f_score()
-            
+        
+        i.set_g(successor_current_cost)
+        q = i
+    open_list.remove(q)
+    closed_list.append(q)
 
 
+print("not finished")
