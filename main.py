87a01f0281f6b482d9876e117183e13336886d50
TomKamstra <tomk2000@hotmail.nl>
2020-01-21 10:08:06 +0100

    Added pseudo to main.py

--- main.py
+++ main.py
@@ -6,18 +6,22 @@ import copy
 
 if __name__ == '__main__':
     # Create netlist by loading file in class
-    netlist = classs.Netlist("data/netlist_4.csv").netlist
+    netlist = classs.Netlist("data/netlist_1.csv").netlist
     print(netlist)
 
     # Create list for gate coordinates
-    gate_coordinates = classs.Gate_coordinate("data/print_2.csv").gate_coordinates
+    gate_coordinates = classs.Gate_coordinate("data/pritn_1.csv").gate_coordinates
     print(gate_coordinates)
 
+    """
+    # TODO
+        geef de begin en eindgate mee
+        alle gate_coordinaten
+        geef een lijst mee met coordinaten waar al draad ligt
+    """ 
+    for net in netlist: 
+        Astarroute = Astar_search(int(net.gate_1))
+        print("From", gate_coordinates[int(net.gate_1)], "To", gate_coordinates[int(net.gate_2)])
 
 
-
-
-    plot.make_grid(5, 5)
-    plot.draw_line([0,0,0], [1,1,1], "red")
-    plot.draw_line([0,0,0], [1,2,1], "blue")
-    plot.plt.show()
+    
